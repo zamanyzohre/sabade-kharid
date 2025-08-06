215434edbaf98bd5883c7be2840310616542d2e7
zohre <zozo.tiktak@gmail.com>
2025-08-06 12:38:01 +0330

    update project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home () {
     <div className='text-center'>
         <h2 className='mb-4'>Shping Card - Webinoo.ir</h2>
         <h5>لطفا برای دیدن محصولات موجود روی منوی </h5>
-         <span>Poducts</span> 
+         <h6>Poducts</h6> 
            <h5>در نوار بالای صفحه کلیک کنید </>h5
     </div>
   )
